73913ca3b643a2591860d7015d9db2b1d8fb9641
Euridice Ingrid <euridiceingrid@gmail.com>
2025-03-03 18:03:40 -0500

    cycles

---
+++ src/cycles/cycle_while.java
@@ -0,0 +1,13 @@
+package cycles;
+
+public class cycle_while {
+    public static void main(String[] args) {
+        System.out.println("****Cycle While****");
+
+        var number = 1;
+
+        while (number <= 6)
+            System.out.println(number++);
+
+    }
+}
---
+++ src/cycles/do_while.java
@@ -0,0 +1,13 @@
+package cycles;
+
+public class do_while {
+    public static void main(String[] args) {
+        System.out.println("****Cycle Do While****");
+
+        var number = 1;
+
+        do {
+            System.out.println(number++);
+        } while (number <= 4);
+    }
+}

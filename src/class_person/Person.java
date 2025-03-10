6cdcb41bc98170ccd180d6c752a2a2c6da15e587
Euridice Ingrid <euridiceingrid@gmail.com>
2025-03-10 18:15:32 -0500

    person class

--- src/class_person/Person.java
+++ src/class_person/Person.java
@@ -1,4 +1,32 @@
 package class_person;
 
 public class Person {
+    private String name;
+    private String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    void showPerson() {
+        System.out.println("Name: " + name);
+        System.out.println("Last Name: " + lastName);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- src/class_person/PersonMain.java
+++ src/class_person/PersonMain.java
@@ -1,4 +1,19 @@
 package class_person;
 
 public class PersonMain {
+    public static void main(String[] args) {
+        System.out.println("****Class Person****");
+
+        var person = new Person("John", "Doe");
+        person.showPerson();
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Last Name: " + person.getLastName());
+
+        person.setName("Jane");
+        person.setLastName("Doe");
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Last Name: " + person.getLastName());
+    }
 }
